a82f27aa4391048780719c16b6d846f30ef095af
Johan <johan.peraldi@gmail.com>
2022-02-09 16:29:58 +0100

    Add .env file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const axios = require("axios").default;
-const dotenv = require("dotenv").config();
+require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
 const app = express();
@@ -12,7 +12,7 @@ app.get("/word", (req, res) => {
     params: { count: "5", wordLength: "5" },
     headers: {
       "x-rapidapi-host": "random-words5.p.rapidapi.com",
-      "x-rapidapi-key": "78f33bb81dmsha759a187650b888p194961jsn3d74783adfb2",
+      "x-rapidapi-key": process.env.RAPID_API_KEY,
     },
   };
   axios
